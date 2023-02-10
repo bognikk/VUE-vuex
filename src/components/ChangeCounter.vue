15240fc9ddb386a8b6b1633e55d7bdb5cbf905a4
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-02-10 16:43:39 -0500

    mappers

--- src/components/ChangeCounter.vue
+++ src/components/ChangeCounter.vue
@@ -1,16 +1,27 @@
 <template>
-	<button @click="addOne">Add 2</button>
+	<button @click="increment">Add 2</button>
+	<button @click="increase({ value: 11 })">Add 11</button>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
 	methods: {
-		addOne() {
-			// this.$store.state.counter = this.$store.state.counter + 1;
-			// this.$store.commit('incrementMutation');
+		// addOne() {
+		// 	// this.$store.state.counter = this.$store.state.counter + 1;
+		// 	// this.$store.commit('incrementMutation');
 
-			this.$store.dispatch('incrementAction');
-		},
+		// 	// this.$store.dispatch('incrementAction');
+		// },
+
+		// ...mapActions(['increaseAction', 'incrementAction']),
+
+		// naming mapped actions
+		...mapActions({
+			increment: 'incrementAction',
+			increase: 'increaseAction',
+		}),
 	},
 };
 </script>
