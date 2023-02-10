60a1f1c3ed3604b423382a1458c28f856a99160e
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-02-10 12:16:37 -0500

    creating and using store

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-	<base-container title="Vuex"></base-container>
+	<base-container title="Vuex">
+		<h3>{{ $store.state.counter }}</h3>
+		<button>Add 1</button>
+	</base-container>
 </template>
 
 <script>
@@ -12,7 +15,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 * {
 	box-sizing: border-box;
 }
@@ -23,5 +26,26 @@ html {
 
 body {
 	margin: 0;
+}
+
+button,
+a {
+	padding: 0.5rem 2rem;
+	border: 1px solid transparent;
+	border-radius: 12px;
+	text-align: center;
+	font: inherit;
+	font-size: 17px;
+	font-weight: 700;
+	color: #000;
+	text-decoration: none;
+	background-color: #42b883;
+	cursor: pointer;
+
+	&:hover,
+	&:active {
+		background-color: #35495e;
+		color: #fff;
+	}
 }
 </style>
--- src/components/BaseContainer.vue
+++ src/components/BaseContainer.vue
@@ -13,9 +13,10 @@ export default {
 
 <style lang="scss" scoped>
 section {
-	max-width: 30rem;
-	padding: 1rem;
+	max-width: 40rem;
+	padding: 2rem;
 	margin: 2rem auto;
+	border-radius: 12px;
 	box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
 
 	h2 {
